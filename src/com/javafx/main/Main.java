a71583dc75eb9082b6a7af554bed7a4d893b5489
lalo1234321 <edominguezc@toluca.tecnm.mx>
2020-09-12 14:31:54 -0500

    HandlerEvent added

--- src/com/javafx/main/Main.java
+++ src/com/javafx/main/Main.java
@@ -2,20 +2,30 @@
 package com.javafx.main;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class HelloWorld extends Application
+public class Main extends Application
 {
     //Comment to see the diference between the branches in netbeans
     @Override
     public void start(Stage arg0) throws Exception
     {
-        Button btn1=new Button("Hello world");
+        Button btn=new Button("Click me");
+        btn.setOnAction(new EventHandler<ActionEvent>()
+        { 
+            @Override
+            public void handle(ActionEvent arg0)
+            {
+                System.out.println("You clicked the button");
+            }
+        });
         StackPane root=new StackPane();
-        root.getChildren().add(btn1);
+        root.getChildren().add(btn);
         Scene scene=new Scene(root,600,400);
         arg0.setScene(scene);
         arg0.setTitle("First JavaFx Application");
