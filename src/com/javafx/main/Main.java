f355016ab0486f007f3f855092c4255c8df11f7b
lalo1234321 <edominguezc@toluca.tecnm.mx>
2020-09-12 17:10:23 -0500

    Lambda expression and exit button added

--- src/com/javafx/main/Main.java
+++ src/com/javafx/main/Main.java
@@ -7,6 +7,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application
@@ -16,6 +17,11 @@ public class Main extends Application
     public void start(Stage arg0) throws Exception
     {
         Button btn=new Button("Click me");
+        Button exitBtn=new Button("Exit");
+        exitBtn.setOnAction(e -> {
+            System.out.println("The program is closing");
+            System.exit(0);
+        });
         btn.setOnAction(new EventHandler<ActionEvent>()
         { 
             @Override
@@ -24,8 +30,8 @@ public class Main extends Application
                 System.out.println("You clicked the button");
             }
         });
-        StackPane root=new StackPane();
-        root.getChildren().add(btn);
+        VBox root=new VBox();
+        root.getChildren().addAll(btn,exitBtn);  
         Scene scene=new Scene(root,600,400);
         arg0.setScene(scene);
         arg0.setTitle("First JavaFx Application");
